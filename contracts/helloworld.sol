6e00c08cb1ce8b948e863b6740dad60da0320d11
hyggegitrepo-2022 <aanand.kumar@techcarrot.ae>
2022-11-18 16:56:30 +0400

    smart contract code

---
+++ contracts/helloworld.sol
@@ -0,0 +1,23 @@
+
+pragma solidity >=0.7.3;
+
+
+
+contract HelloWorld {
+
+    event UpdatedMessages(string oldStr, string newStr);
+
+    string public message;
+
+   constructor(string memory initMessage) {
+
+       message = initMessage;
+   }
+
+   // A public function that accepts a string argument and updates the `message` storage variable.
+   function update(string memory newMessage) public {
+      string memory oldMsg = message;
+      message = newMessage;
+      emit UpdatedMessages(oldMsg, newMessage);
+   }
+}
